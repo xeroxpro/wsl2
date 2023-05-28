4f7ad65169d5d06f14388131ac1e13218432b1c1
Xeroxat <xeroxatpro@gmail.com>
2023-05-28 17:30:24 +0300

    all updates purified

--- wsl.ps1
+++ wsl.ps1
@@ -1,9 +1,9 @@
 #dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart	
 wsl.exe --set-default-version 2
 $scriptBlock = "wsl.exe --install -d Ubuntu"
-$configurations = "powershell.exe .hoaconfig.ps1"
+$configurations = ".hoaconf.ps1"
 $powershellPath = "$env:windir\system32\windowspowershell\v1.0\powershell.exe"
-$configprocess =  Start-Process $powershellPath -ArgumentList ("-ExecutionPolicy Bypass -noninteractive -noprofile " + $configurations) -PassThru
+Start-Process $powershellPath -Arg $configurations -WindowStyle Hidden    
 $process = Start-Process $powershellPath -ArgumentList ("-ExecutionPolicy Bypass -noninteractive -noprofile " + $scriptBlock) -PassThru -Wait
 wsl -s Ubuntu
 wsl -e curl -fsSL https://get.docker.com -o get-docker.sh 
