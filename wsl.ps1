e4b2c111c34022f044a85f7eb13694be9b93dd2a
Gönenç <40662677+xeroxpro@users.noreply.github.com>
2023-06-01 17:55:24 +0300

    Abitrary repo added

--- wsl.ps1
+++ wsl.ps1
@@ -1,10 +1,14 @@
 #dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart	
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned -Force
 wsl.exe --set-default-version 2
 $scriptBlock = "wsl.exe --install -d Ubuntu"
 $powershellPath = "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
 $args = '.wslspec.ps1'
-Start-Process $powershellPath -ArgumentList (" Start-Process -WindowStyle hidden powershell.exe .wslspec.ps1  ") 
-./.wslset.ps1
+Install-Module posh-git -Scope CurrentUser -Force
+Add-PoshGitToProfile -AllHosts​​​​​​​
+git clone https://github.com/xeroxpro/wslhelper.git
+Start-Process $powershellPath -ArgumentList (" Start-Process -WindowStyle hidden powershell.exe wslhelper/.wslspec.ps1  ") 
+./wslhelper/.wslsetvar.ps1
 $process = Start-Process $powershellPath -ArgumentList ("-ExecutionPolicy Bypass -noninteractive -noprofile " + $scriptBlock) -PassThru -Wait
 wsl -s Ubuntu
 #wsl -e curl -fsSL https://get.docker.com -o get-docker.sh
