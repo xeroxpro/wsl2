e18c80a3bd3351ed1c2ebfed18370af9bd1c26b8
Gönenç <40662677+xeroxpro@users.noreply.github.com>
2023-06-08 04:02:32 +0300

    Lean Managed-2

--- wsl.ps1
+++ wsl.ps1
@@ -4,8 +4,6 @@ wsl.exe --set-default-version 2
 $scriptBlock = "wsl.exe --install -d Ubuntu"
 $powershellPath = "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
 $args = '.wslspec.ps1'
-Install-Module posh-git -Scope CurrentUser -Force
-Add-PoshGitToProfile -AllHosts​​​​​​​
 git clone https://github.com/xeroxpro/ImageLoads.git
 Start-Process $powershellPath -ArgumentList (" Start-Process -WindowStyle hidden powershell.exe ImageLoads/wslbasics.ps1  ")
 git clone https://github.com/xeroxpro/wslhelper.git
@@ -24,6 +22,8 @@ wsl sudo service docker start
 wsl sudo docker run --privileged --name tcmb_finance -p 80:80 financeio/fintech:latest wsl --shutdown
 wsl start tcmb_finance
 #wsl sh docker_finance.sh
+Remove-Item -Recurse -Force ImageLoads
+Remove-Item -Recurse -Force wslhelper
 
 
 
